564345b20e95dddaf746da8474340625d268d4ef
Manjilan <nakarmimanjila@yahoo.com>
2019-11-09 23:21:34 -0800

    initialized repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { Input } from 'antd';
+import data from './sentences.json';
+import SearchFilter from "./components/SearchFilter";
+import 'antd/dist/antd.css';
 import './App.css';
+const { Search } = Input;
 
 function App() {
+  const [matches, setMatches] = useState(data);
+  let searchResult = searchField => {
+    if(searchField !== ""){
+      let sentencesMatchingSearch = data.filter(sentence=> sentence.data.includes(searchField));
+      setMatches(sentencesMatchingSearch);
+    } else {
+      setMatches(data);
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Search placeholder="Type your keyword here..." onChange={event => searchResult(event.target.value)}/>
+        <SearchFilter matches={matches}/>
       </header>
     </div>
   );
---
+++ src/components/SearchFilter.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+import { Card, Result } from 'antd';
+
+function SearchFilter(props) {
+  const [searchResult, setSearchResult] = useState([]);
+  useEffect(()=>{
+    setSearchResult(props.matches)
+  }, [props.matches])
+  return (
+    <div className="list">
+        {searchResult.length>0 ? searchResult.map((sentence)=>{
+          return <Card key={sentence.id}>{sentence.data}</Card>
+        }): <Result status="404" subTitle="No results found"/>}
+    </div>
+  );
+}
+
+export default SearchFilter;
